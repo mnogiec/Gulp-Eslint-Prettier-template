6b1da258b422f0fddddc690a671f2087ea2f826b
mnogiec <nogiecmikolaj@gmail.com>
2019-05-03 20:13:34 +0200

    Create gulpfile.js template

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+// Initialize modules
+const { src, dest, watch, series, parallel } = require('gulp');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const concat = require('gulp-concat');
+const postcss = require('gulp-postcss');
+const sass = require('gulp-sass');
+
+// File path variables
+const files = {
+  sassPath: 'src/sass/**/*.scss',
+  jsPath: 'src/js/**/*.js'
+};
+
+// Sass task
+const sassTask = () => {
+  return src(files.sassPath)
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(dest('dist'));
+};
+
+// JS task
+const jsTask = () => {
+  return src(files.jsPath)
+    .pipe(concat('scripts.js'))
+    .pipe(dest('dist'));
+};
+
+// Watch task
+const watchTask = () => {
+  watch([files.sassPath, files.jsPath], parallel(sassTask, jsTask));
+};
+
+// Default task
+exports.default = series(parallel(sassTask, jsTask), watchTask);
